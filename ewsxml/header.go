16caa08a1e1e3080388fc27d2c653e8bf31b3822
roel.schut <roel.schut@abovo.nl>
2022-05-02 15:49:46 +0200

    Fix ewsxml.Header
    ExchangeImpersonation is now a pointer and should be nil when no impersonation is required.

--- ewsxml/header.go
+++ ewsxml/header.go
@@ -9,34 +9,34 @@ type Version string
 func (v Version) String() string { return string(v) }
 
 type Header struct {
-	XMLName               xml.Name `xml:"soap:Header"`
-	RequestServerVersion  RequestServerVersion
-	ExchangeImpersonation ExchangeImpersonation
+	XMLName               xml.Name               `xml:"soap:Header"`
+	RequestServerVersion  RequestServerVersion   `xml:"t:RequestServerVersion"`
+	ExchangeImpersonation *ExchangeImpersonation `xml:"t:ExchangeImpersonation,omitempty"`
 }
 
-func (h *Header) SetVersion(ver Version) {
+func (h *Header) ServerVersion(ver Version) {
 	h.RequestServerVersion.Version = ver
 }
 
-func (h *Header) DiscardImpersonation() {
-	h.ExchangeImpersonation = ExchangeImpersonation{}
-}
+func (h *Header) DiscardImpersonation() { h.ExchangeImpersonation = nil }
 
 func (h *Header) ImpersonateSmtpAddress(v string) {
-	h.ExchangeImpersonation.ConnectingSID.SmtpAddress = v
+	h.ExchangeImpersonation = &ExchangeImpersonation{
+		ConnectingSID: ConnectingSID{SmtpAddress: v},
+	}
 }
 
 func (h *Header) ImpersonatePrimarySmtpAddress(v string) {
-	h.ExchangeImpersonation.ConnectingSID.PrimarySmtpAddress = v
+	h.ExchangeImpersonation = &ExchangeImpersonation{
+		ConnectingSID: ConnectingSID{PrimarySmtpAddress: v},
+	}
 }
 
 type RequestServerVersion struct {
-	XMLName xml.Name `xml:"t:RequestServerVersion"`
-	Version Version  `xml:"Version,attr"`
+	Version Version `xml:"Version,attr"`
 }
 
 type ExchangeImpersonation struct {
-	XMLName       xml.Name      `xml:"t:ExchangeImpersonation"`
 	ConnectingSID ConnectingSID `xml:"t:ConnectingSID,omitempty"`
 }
 
