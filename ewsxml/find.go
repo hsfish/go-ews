37f2f91258ee7beef0b1ba195ad09c6c1b68163b
Леонид Криницын <lkrinitsyn@stsoft.ru>
2022-09-09 17:03:54 +0300

    Updated received Body namespace

--- ewsxml/find.go
+++ ewsxml/find.go
@@ -64,7 +64,7 @@ const (
 // The Body element specifies the body of an item.
 // https://docs.microsoft.com/en-us/exchange/client-developer/web-service-reference/body
 type Body struct {
-	XMLName     xml.Name `xml:"t:Body"`
+	XMLName     xml.Name `xml:"Body"`
 	BodyType    BodyType `xml:"BodyType,attr"`
 	IsTruncated bool     `xml:"IsTruncated,attr"`
 	Contents    []byte   `xml:",chardata"`
